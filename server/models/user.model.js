2addebfdce3463ffa8b5e331e456c8c522a934b6
Samuel Kabuya <samuelkabuya408@gmail.com>
2025-10-29 10:33:34 -0400

    Stll testing

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
+
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     trim: true,
     required: "Name is required",
   },
+
   email: {
     type: String,
     trim: true,
@@ -12,20 +14,24 @@ const UserSchema = new mongoose.Schema({
     match: [/.+\@.+\..+/, "Please fill a valid email address"],
     required: "Email is required",
   },
+
   created: {
     type: Date,
     default: Date.now,
   },
+
   updated: {
     type: Date,
     default: Date.now,
   },
+
   hashed_password: {
     type: String,
     required: "Password is required",
   },
   salt: String,
 });
+
 UserSchema.virtual("password")
   .set(function (password) {
     this._password = password;
@@ -36,6 +42,7 @@ UserSchema.virtual("password")
   .get(function () {
     return this._password;
   });
+  
 UserSchema.path("hashed_password").validate(function (v) {
   if (this._password && this._password.length < 6) {
     this.invalidate("password", "Password must be at least 6 characters.");
