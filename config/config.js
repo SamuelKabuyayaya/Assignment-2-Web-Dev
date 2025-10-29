e22837637566d2eeafcb49b1a659c4a2af0f027f
Samuel Kabuya <samuelkabuya408@gmail.com>
2025-10-29 10:14:24 -0400

    Push test

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
  const config = {
  env: process.env.NODE_ENV || 'development', 
- port: process.env.PORT || 5000,
+ port: process.env.PORT || 5173,
  jwtSecret: process.env.JWT_SECRET || "I5aeDqp20NEFy21P", 
  mongoUri: process.env.MONGODB_URI ||"mongodb+srv://kakapuka677_db_user:I5aeDqp20NEFy21P@cluster0.gvbjh9v.mongodb.net/"||
  process.env.MONGO_HOST ||
