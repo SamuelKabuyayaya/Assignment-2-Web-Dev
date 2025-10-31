60245c79c196dd699dab8d722504670cfbe39a0b
Samuel Kabuya <samuelkabuya408@gmail.com>
2025-10-31 10:07:35 -0400

    Switched my database name to Portfolio

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@
  env: process.env.NODE_ENV || 'development', 
  port: process.env.PORT || 5173,
  jwtSecret: process.env.JWT_SECRET || "I5aeDqp20NEFy21P", 
- mongoUri: process.env.MONGODB_URI ||"mongodb+srv://kakapuka677_db_user:I5aeDqp20NEFy21P@cluster0.gvbjh9v.mongodb.net/"||
+ mongoUri: process.env.MONGODB_URI ||"mongodb+srv://kakapuka677_db_user:I5aeDqp20NEFy21P@cluster0.gvbjh9v.mongodb.net/Portfolio"||
  process.env.MONGO_HOST ||
  'mongodb://' + (process.env.IP || 'localhost') + ':' + 
 (process.env.MONGO_PORT || '27017') +
